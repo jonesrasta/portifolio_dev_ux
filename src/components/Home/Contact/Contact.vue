f7bc681950b37d7c0432d3a0dbd1a5b750bfd57e
Jones Lima <jonesrasta@gmail.com>
2023-11-16 11:00:54 -0300

    feat: ajustes input forms

--- src/components/Home/Contact/Contact.vue
+++ src/components/Home/Contact/Contact.vue
@@ -2,34 +2,34 @@
 </script>
 
 <template>
-    <div class="container bg-[#0D1016] mb-10 mx-auto px-8">
+    <div class="container bg-[#0D1016] mb-8 mx-auto px-8">
         <div
-            class=" mt-24 px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto bg-[#161A22] text-white rounded-lg shadow-lg">
+            class="mt-20 px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-14 mx-auto bg-[#161A22] text-white rounded-lg shadow-lg">
             <div class="flex flex-col justify-between">
                 <div>
                     <h2 class="text-3xl font-bold leading-tight">Lets talk about everything!</h2>
                 </div>
-                <div class="text-center order-2">
-                    <!-- <img src="/src/assets/img/jones5.png" alt="" class="-ml-10"> -->
+                <div class="mt-8 text-center order-2">
+                    <img src="/src/assets/img/jones5.png" alt="" class="-ml-10 max-md:hidden">
                 </div>
             </div>
-            <div class="my-8 text-black">
+            <div class="my-4 text-black max-sm:my-0">
                 <div>
                     <span class="text-lg text-gray-400 font-bold">Company Name</span>
                     <input
-                        class="w-full bg-gray-300 text-gray-900 mt-2 p-2 rounded-md focus:outline-none focus:shadow-outline focus:ring-[#6cd698]"
+                        class="w-full bg-[#ffffff17] text-white mt-2 p-2 rounded-md focus:outline-none focus:shadow-outline focus:ring-[#6cd698]"
                         type="text" placeholder="">
                 </div>
                 <div class="mt-8">
                     <span class="text-lg text-gray-400 font-bold">Email</span>
                     <input
-                        class="w-full bg-gray-300 text-gray-900 mt-2 p-2 rounded-md focus:outline-none focus:shadow-outline focus:ring-[#6cd698]"
+                        class="w-full bg-[#ffffff17] text-white mt-2 p-2 rounded-md focus:outline-none focus:shadow-outline focus:ring-[#6cd698]"
                         type="text">
                 </div>
                 <div class="mt-8">
                     <span class="text-lg text-gray-400 font-bold">Message</span>
                     <textarea
-                        class="w-full h-32 bg-gray-300 text-gray-900 mt-2 p-2 rounded-md focus:outline-none focus:shadow-outline focus:ring-[#6cd698]"></textarea>
+                        class="w-full h-32 bg-[#ffffff17] text-white mt-2 p-2 rounded-md focus:outline-none focus:shadow-outline focus:ring-[#6cd698]"></textarea>
                 </div>
                 <button
                     class="text-lg font-medium tracking-wide rounded-full py-3 w-full my-6 bg-[#6FF1A6] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)]">
--- src/components/Home/Download/Download.vue
+++ src/components/Home/Download/Download.vue
@@ -8,14 +8,14 @@
     <div
       class="bg-[#161A22] rounded-md shadow flex-col max-sm:p-8 flex items-center justify-center mx-8 h-80 max-sm:h-96 max-sm:mx-8">
       <p
-        class="flex flex-col justify-center max-sm:w-80 max-sm:px-6 content-center w-[860px] my-10 text-gray-400 font-thin text-base">
+        class="flex flex-col justify-center max-sm:w-80 max-sm:px-6 content-center w-[860px] my-10 max-lg:w-[460px] text-gray-400 font-thin text-lg max-sm:text-base">
         Fique à vontade para baixar o meu currículo em formato PDF e obter
         uma visão mais completa da minha carreira e habilidades. Estou ancioso
         para discutir oportunidades de trabalho, e como posso contribuir para o
         sucesso da sua empresa.
       </p>
       <a :href="'/files/Jones Lima.pdf'" download target="blank" type="button"
-        class="bg-[#6FF1A6] flex items-center rounded-full bg-primary py-3 px-5 text-lg font-medium transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)]">
+        class="bg-[#6FF1A6] flex items-center rounded-full bg-primary py-3 px-8 text-lg font-medium transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)]">
         Download CV
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-1 h-6 w-6 mx-2">
           <path fill-rule="evenodd"
